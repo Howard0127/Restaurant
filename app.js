9ac503838247226fc494b245e8966c81f4d721a1
Howard0127 <howard740127@gmail.ocm>
2021-09-10 23:35:01 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,40 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+const restaurantList = require('./restaurant.json')
+
+// require express-handlebars here
+const exphbs = require('express-handlebars')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+  const restaurants = restaurantList.results
+  res.render('index', { restaurants })
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  const restaurant = restaurantList.results.find(item => item.id.toString() === id)
+  res.render('show', { restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const filteredRestaurant = restaurantList.results.filter(item => {
+    return item.name.toLowerCase().includes(keyword.toLowerCase()) || item.category.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: filteredRestaurant, keyword })
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost: ${port}`)
+})
