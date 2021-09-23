9e6aaa5417951c735e7e95ecdb7338d41ca5f8f7
Howard0127 <howard740127@gmail.ocm>
2021-09-23 15:46:53 +0800

    feat: add sorting function and modify search funcition route

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -15,6 +15,56 @@ router.post('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//route 到搜尋結果頁面
+router.get('/search', (req, res) => {
+  const keyword = req.query.keyword.trim()
+  const keywordArr = keyword.toLowerCase().split(' ')
+  
+  Restaurant.find()
+    .lean()
+    .then(restaurants => {
+      let filteredRestaurant = []
+      for (restaurant of restaurants) {
+        const name = restaurant.name.toLowerCase()
+        const category = restaurant.category.toLowerCase()
+        const location = restaurant.location.toLowerCase()
+        if (keywordArr.find((word) => 
+          name.includes(word) || category.includes(word) || location.includes(word)
+        )) {
+          filteredRestaurant.push(restaurant)
+        }         
+      }
+
+      res.render('index', { restaurants: filteredRestaurant, keyword })
+    })
+    .catch(error => console.log(error))
+})
+
+// sorting routes
+router.get('/new-old', (req, res) => {
+  Restaurant.find()
+  .lean()
+  .sort({ _id: 'desc' })
+  .then(restaurants => res.render('index', { restaurants }))
+  .catch(error => console.error(error))
+})
+
+router.get('/rating', (req, res) => {
+  Restaurant.find()
+  .lean()
+  .sort({ rating: 'desc' })
+  .then(restaurants => res.render('index', { restaurants }))
+  .catch(error => console.error(error))
+})
+
+router.get('/category', (req, res) => {
+  Restaurant.find()
+  .lean()
+  .sort({ category: 'asc' })
+  .then(restaurants => res.render('index', { restaurants }))
+  .catch(error => console.error(error))
+})
+
 //route 編輯某餐廳頁面
 router.get('/:id/edit', (req, res) => {
   const id = req.params.id
