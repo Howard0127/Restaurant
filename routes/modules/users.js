60f14eec228561ad1a8c1e727843394a0675d0b1
Howard0127 <howard740127@gmail.ocm>
2021-10-22 20:26:02 +0800

    feat: add lotout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -10,7 +10,7 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
-  failureRedirect: 'users/login'
+  failureRedirect: '/users/login'
 }))
 
 router.get('/register', (req, res) => {
@@ -39,6 +39,11 @@ router.post('/register', (req, res) => {
         .catch(err => console.log(err))
     }
   })
+})
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
 })
 
 module.exports = router
